84612da5a74d31a525512d7072996e8d9f0ba790
daksh-goyal7 <149812584+daksh-goyal7@users.noreply.github.com>
2024-01-15 14:43:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app=Flask("Website")
+
+stations=pd.read_csv("data/stations.txt",skiprows=17)
+stations=stations[["STAID","STANAME                                 "]][:92]
+
+@app.route("/")
+def home():
+    return render_template("home.html",data=stations.to_html())
+
+@app.route("/api/v1/<station>/<date>")
+def about(station,date):
+    filename="data/TG_STAID"+str(station).zfill(6)+".txt"
+    df=pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+    temperature=df.loc[df['    DATE']==date]['   TG'].squeeze()/10
+    return {"station":station,
+            "date":date,
+            "temperature":temperature
+    }
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df=pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+    result=df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"]=df["    DATE"].astype(str)
+    result=df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
+app.run(debug=True)
